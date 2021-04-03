9331326d9a76cd39d9515ecb42e0696b6407af80
Sconzo <rspolydoro@gmail.com>
2021-04-02 21:14:12 -0300

    Criando a classe "Conta"

---
+++ SconzoBank/Conta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SconzoBank
+{
+    class Conta
+    {
+        public int NumeroConta { get; set; }
+        public int NumeroAgencia { get; set; }
+        public double Saldo { get; set; }
+
+        public Conta(int numero_conta,int numero_agencia,double deposito_inicial)
+        {
+            NumeroAgencia = numero_agencia;
+            NumeroConta = numero_conta;
+            Saldo = deposito_inicial;
+        }
+    }
+}
--- SconzoBank/Program.cs
+++ SconzoBank/Program.cs
@@ -10,6 +10,7 @@ namespace SconzoBank
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
